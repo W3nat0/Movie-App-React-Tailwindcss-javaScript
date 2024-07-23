7c45e705c992fdb8741099fdbb8c71e6454d7364
Arman <arman.2019.mkrtchyan@gmail.com>
2024-07-23 17:39:29 +0400

    fix: provider

--- src/provider/store/cartSlice/index.js
+++ src/provider/store/cartSlice/index.js
@@ -23,13 +23,3 @@ export const { addCart, removeCart, clearCart } = cartSlice.actions;
 export const getCartItems = (state) => state.cart.items;
 
 export default cartSlice.reducer;
-{
-  /* <Dropdown
-  title={
-    <span className="flex items-center text-xl hover:text-yellow-500">
-      <FaStream className="mr-2" /> Movie
-    </span>
-  }
-  items={items}
-/>; */
-}
